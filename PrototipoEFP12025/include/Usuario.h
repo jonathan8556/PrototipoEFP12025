12bf329b175c4cedde2667ef1f863a6a75b698ef
Jonathan Samuel Gonzalez <jgonzalezi7@miumg.edu.gt>
2025-05-28 11:21:49 -0600

    RamaSeguridadJOnathanGonzalez

---
+++ PrototipoEFP12025/include/Usuario.h
@@ -0,0 +1,16 @@
+#ifndef USUARIO_H
+#define USUARIO_H
+
+
+class Usuario
+{
+    public:
+        Usuario();
+        virtual ~Usuario();
+
+    protected:
+
+    private:
+};
+
+#endif // USUARIO_H
